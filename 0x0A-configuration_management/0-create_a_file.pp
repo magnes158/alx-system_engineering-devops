e41c461c2dd75a881bc982adf7e19e4cccfc904b
magnes158 <mohamedabdiali158@gmail.com>
2023-03-24 07:13:29 -0700

    kill

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# This Puppet manifest creates a file in /tmp
+# creates a file in /tmp
 
-file { 'holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+file { '/tmp/school':
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
